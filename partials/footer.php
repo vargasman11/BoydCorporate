c9fd74f03aaca3ca7fd3979763e2358d3cd39bca
Jerry Vargas <jvargas@conversionia.com>
2019-06-27 15:41:46 -0500

    6-27-2019 progress

--- partials/footer.php
+++ partials/footer.php
@@ -1,20 +1,39 @@
-
+<div class="container" id="footer">
+    <div class="row">
+        <div class="col-sm-12 col-md-8">
+            <img src="Assests/logos/smartway-logo.png" alt="" id="smartway-logo">
+            <p id="copyright"> Copyright 2019, All Rights Reserved &reg; <br>
+            Boyd Bros. Transportation, Inc.
+        </p>
+        <a href="#" id="privacy-policy">Privcacy Policy</a>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <a href="#">Boyd Bros. Driver Site</a>
+            <h4>Corporate Headquarters</h4>
+            <p id="address">3275 Highway 30 <br>
+            Clayton, AL 36016 <br>
+            Phone:800-633-1502</p>
+        </div>
+    </div>
 </div>
-    <!-- Optional JavaScript -->
-    <!-- <script src="scripts/main.js"></script> -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
 
-    <!-- Custom jquery -->
-    <script src="scripts/script.js"></script>
+    <!-- end body -->
+</div>
+<!-- Optional JavaScript -->
+<!-- <script src="scripts/main.js"></script> -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+
+<!-- Custom jquery -->
+<script src="scripts/script.js"></script>
 </body>
 
 </html>
